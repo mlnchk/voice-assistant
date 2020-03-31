2edd5fb694c83c1321aeb458c5f5e8a9df845dc1
mlnchk <danil.mlnchk@gmail.com>
2020-03-31 15:18:13 +0300

    feat: separate cli

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
-#!/usr/bin/env python3
-"""Create a recording with arbitrary duration.
-
-PySoundFile (https://github.com/bastibe/PySoundFile/) has to be installed!
-
-"""
 import argparse
 import tempfile
 import queue
 import sys
+import sounddevice as sd
+import soundfile as sf
 from silence import remove_silence
-
 
 def int_or_str(text):
     """Helper function for argument parsing."""
@@ -36,42 +31,47 @@ parser.add_argument(
     '-t', '--subtype', type=str, help='sound file subtype (e.g. "PCM_24")')
 args = parser.parse_args()
 
-try:
-    import sounddevice as sd
-    import soundfile as sf
+def get_devices():
+    devices = sd.query_devices()
+    return devices
 
-    if args.list_devices:
-        print(sd.query_devices())
+def cli():
+    try:
+        if args.list_devices:
+            print(get_devices())
+            parser.exit(0)
+        if args.samplerate is None:
+            device_info = sd.query_devices(args.device, 'input')
+            # soundfile expects an int, sounddevice provides a float:
+            args.samplerate = int(device_info['default_samplerate'])
+        if args.filename is None:
+            args.filename = tempfile.mktemp(prefix='rec_unlimited_',
+                                            suffix='.wav', dir='')
+        q = queue.Queue()
+
+        def callback(indata, frames, time, status):
+            """This is called (from a separate thread) for each audio block."""
+            if status:
+                print(status, file=sys.stderr)
+            q.put(indata.copy())
+
+        # Make sure the file is opened before recording anything:
+        with sf.SoundFile(args.filename, mode='x', samplerate=args.samplerate,
+                        channels=args.channels, subtype=args.subtype) as file:
+            with sd.InputStream(samplerate=args.samplerate, device=args.device,
+                                channels=args.channels, callback=callback):
+                print('#' * 80)
+                print('press Ctrl+C to stop the recording')
+                print('#' * 80)
+                while True:
+                    file.write(q.get())
+
+    except KeyboardInterrupt:
+        print('\nRecording finished: ' + repr(args.filename))
+        remove_silence(args.filename)
         parser.exit(0)
-    if args.samplerate is None:
-        device_info = sd.query_devices(args.device, 'input')
-        # soundfile expects an int, sounddevice provides a float:
-        args.samplerate = int(device_info['default_samplerate'])
-    if args.filename is None:
-        args.filename = tempfile.mktemp(prefix='rec_unlimited_',
-                                        suffix='.wav', dir='')
-    q = queue.Queue()
+    except Exception as e:
+        parser.exit(type(e).__name__ + ': ' + str(e))
 
-    def callback(indata, frames, time, status):
-        """This is called (from a separate thread) for each audio block."""
-        if status:
-            print(status, file=sys.stderr)
-        q.put(indata.copy())
-
-    # Make sure the file is opened before recording anything:
-    with sf.SoundFile(args.filename, mode='x', samplerate=args.samplerate,
-                      channels=args.channels, subtype=args.subtype) as file:
-        with sd.InputStream(samplerate=args.samplerate, device=args.device,
-                            channels=args.channels, callback=callback):
-            print('#' * 80)
-            print('press Ctrl+C to stop the recording')
-            print('#' * 80)
-            while True:
-                file.write(q.get())
-
-except KeyboardInterrupt:
-    print('\nRecording finished: ' + repr(args.filename))
-    remove_silence(args.filename)
-    parser.exit(0)
-except Exception as e:
-    parser.exit(type(e).__name__ + ': ' + str(e))
+if __name__ == "__main__":
+    cli()
