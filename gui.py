7cd66c28492aa6b71d04a2303b19babf846da3da
EugeneOne1 <gubaldulnzulfia@gmail.com>
2020-04-02 22:37:48 +0500

    Basic GUI layput

--- gui.py
+++ gui.py
@@ -11,10 +11,80 @@ class AppCore(wx.App):
 class MainFrame (wx.Frame):
 
     def __init__ (self):
-        wx.Frame.__init__(self, 
+        wx.Frame.__init__(self,
                           parent = None,
                           style = wx.CAPTION | wx.CLOSE_BOX | wx.MINIMIZE_BOX)
-        self.pan = wx.Panel(self)
+        self.SetTitle('Voice Assistant')
+        self.panel = wx.Panel(self)
+        self.__SetLayout()
+        self.__Bind()
+
+    def __SetLayout (self):
+        panel = self.panel
+
+        sizers = [
+            {
+                'object' : wx.BoxSizer(wx.VERTICAL),
+                'style' : { 'flag' : wx.ALL | wx.EXPAND, 'border' : 3 }
+            },
+            {
+                'object' : wx.BoxSizer(wx.VERTICAL),
+                'style' : { 'flag' : wx.ALL | wx.EXPAND, 'border' : 3 }
+            },
+            {
+                'object' : wx.BoxSizer(wx.HORIZONTAL),
+                'separator' : {
+                    'object' : wx.StaticLine(panel),
+                    'style'  : { 'flag' : wx.LEFT | wx.RIGHT | wx.EXPAND, 'border' : 6 }
+                },
+                'style' : { 'proportion' : 1, 'flag' : wx.ALL | wx.EXPAND, 'border' : 3 }
+            }
+        ]
+        widgets = [
+            {
+                'object' : wx.StaticText(panel, name = 'SessionNameLabel', label = 'Session name:'),
+                'sizer' : 1
+            },
+            {
+                'object' : wx.TextCtrl(panel, name = 'SessionNameTextControl'),
+                'sizer' : 1
+            },
+            {
+                'object' : wx.Button(panel, name = 'recBtn'),
+                'sizer'  : 2
+            },
+            {
+                'object' : wx.Button(panel, name = 'stopBtn'),
+                'sizer'  : 2
+            },
+            {
+                'object' : wx.Button(panel, name = 'playBtn'),
+                'sizer'  : 2
+            }
+        ]
+
+        for widget in widgets:
+            if 'style' not in widget.keys():
+                widget['style'] = sizers[widget['sizer']]['style']
+            sizers[widget['sizer']]['object'].Add(widget['object'], **widget['style'])
+        
+        for sizer in sizers[1:]:
+            if 'separator' in sizer.keys():
+                sizers[0]['object'].Add(sizer['separator']['object'], **sizer['separator']['style'])
+            sizers[0]['object'].Add(sizer['object'], **sizers[0]['style'])
+
+        panel.SetSizer(sizers[0]['object'])
+
+    def __Bind(self):
+        #panel = self.panel
+
+        self.Bind(wx.EVT_PAINT, self.__OnRepaint)
+
+    def __OnRepaint (self, event):
+        #self.__SetContent(self.panel)
+        sizer = self.panel.GetSizer()
+        sizer.Layout()
+        sizer.Fit(self)
 
 # test part
 
