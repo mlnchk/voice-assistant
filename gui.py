1d43941ab6e216d102f5b7e8860d54fc7250c841
EugeneOne1 <gubaldulnzulfia@gmail.com>
2020-04-03 12:40:38 +0500

    fucking tuples fixed

--- gui.py
+++ gui.py
@@ -8,20 +8,20 @@ class AppCore(wx.App):
         self.SetTopWindow(mf)
         return True
 
-class ElemGUI ():
+class gui ():
     def __init__ (self, obj, style = None, child = None):
-        self.obj = obj,
-        self.style = style,
+        self.obj = obj
+        self.style = style
         self.child = child
     
     def Compose(self, parent = None):
-        if self.style == (None,):
+        if self.style == None:
             self.style = parent.style
         if self.child != None:
             for c in self.child:
                 c.Compose(self)
         if parent != None:
-            parent.obj[0].Add(self.obj[0], **self.style[0])
+            parent.obj.Add(self.obj, **self.style)
 
 class MainFrame (wx.Frame):
 
@@ -37,48 +37,48 @@ class MainFrame (wx.Frame):
     def __SetLayout (self):
         panel = self.panel
 
-        elements = ElemGUI(
+        elements = gui(
             wx.BoxSizer(wx.VERTICAL),
             { 'flag' : wx.ALL | wx.EXPAND, 'border' : 3 },
             [
-                ElemGUI(
+                gui(
                     wx.BoxSizer(wx.VERTICAL),
                     { 'flag' : wx.ALL | wx.EXPAND, 'border' : 3 },
                     [
-                        ElemGUI(wx.StaticText(panel, name = 'SessionNameLabel', label = 'Session name:')),
-                        ElemGUI(wx.TextCtrl(panel, name = 'SessionNameTextControl'))
+                        gui(wx.StaticText(panel, name = 'SessionNameLabel', label = 'Session name:')),
+                        gui(wx.TextCtrl(panel, name = 'SessionNameTextControl'))
                     ]
                 ),
-                ElemGUI(
+                gui(
                     wx.StaticLine(panel),
                     { 'flag' : wx.LEFT | wx.RIGHT | wx.EXPAND, 'border' : 6 }
                 ),
-                ElemGUI(
+                gui(
                     wx.BoxSizer(wx.VERTICAL),
                     { 'flag' : wx.ALL | wx.EXPAND, 'border' : 3 },
                     [
-                        ElemGUI(wx.StaticText(panel, name = 'InputDeviceLabel', label = 'Input device:')),
-                        ElemGUI(wx.Choice(panel, name = 'InputDeviceChoice'))
+                        gui(wx.StaticText(panel, name = 'InputDeviceLabel', label = 'Input device:')),
+                        gui(wx.Choice(panel, name = 'InputDeviceChoice'))
                     ]
                 ),
-                ElemGUI(
+                gui(
                     wx.StaticLine(panel),
                     { 'flag' : wx.LEFT | wx.RIGHT | wx.EXPAND, 'border' : 6 }
                 ),
-                ElemGUI(
+                gui(
                     wx.BoxSizer(wx.HORIZONTAL),
                     { 'proportion' : 1, 'flag' : wx.ALL | wx.EXPAND, 'border' : 3 },
                     [
-                        ElemGUI(wx.Button(panel, name = 'recBtn', label = 'record')),
-                        ElemGUI(wx.Button(panel, name = 'stopBtn', label = 'stop')),
-                        ElemGUI(wx.Button(panel, name = 'playBtn', label = 'play'))
+                        gui(wx.Button(panel, name = 'recBtn', label = 'record')),
+                        gui(wx.Button(panel, name = 'stopBtn', label = 'stop')),
+                        gui(wx.Button(panel, name = 'playBtn', label = 'play'))
                     ]
                 ),
             ]
         )
         elements.Compose()
 
-        panel.SetSizer(elements.obj[0])
+        panel.SetSizer(elements.obj)
 
         sb = self.CreateStatusBar(name = 'statusBar')
         sb.SetStatusText('Sample state')
