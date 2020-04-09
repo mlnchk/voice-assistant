1af0b1828f9c248bf6fd6f756cabb780cf2b4455
EugeneOne1 <gubaldulnzulfia@gmail.com>
2020-04-10 02:01:06 +0500

    some fucking little changes for building exe

--- globals.py
+++ globals.py
@@ -1,6 +1,7 @@
 import wx
 import json
 import os.path
+import sys
 
 AppIconBitmap = None
 Content = None
@@ -10,6 +11,13 @@ Settings = {
     'pathname_regexp' : (True, r'.*/(.*)_assignsubmission_file_/.*', 1),
     'rendering_quality' : 0
 }
+
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
 
 def CheckExisting(filePath):
     if filePath == None:
@@ -35,11 +43,11 @@ def LoadResources():
 
 def loadIcon():
     global AppIconBitmap
-    AppIconBitmap = wx.Bitmap('gui_resources/icon.ico')
+    AppIconBitmap = wx.Bitmap(resource_path('gui_resources/icon.ico'))
 
 def loadContent():
     global Content
-    contentFile = OpenFile('gui_resources/content.json')
+    contentFile = OpenFile(resource_path('gui_resources/content.json'))
     if contentFile != None:
         Content = json.loads(contentFile)
 
